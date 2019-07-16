86b3d07c87f59a02028d41d6cf679e4a26af07cd
ruslansleekr <ruslan@sleekr.co>
2019-07-16 08:41:26 +0700

    create stock html & array

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,43 @@
 <template>
-  <h1>The Stocks - stocks components</h1>
+  <div class="col-sm-6 col-md-4 mb-3">
+    <div class="card">
+      <div class="card-header text-left bg-info text-white">
+        <p class="mb-0">
+          {{ stock.name }}
+          <span>(Price: {{ stock.price }})</span>
+        </p>
+      </div>
+      <div class="card-body">
+        <div class="float-left">
+          <input type="number"
+                  class="form-control"
+                  placeholder="Quantity"
+                  v-model="quantity">
+        </div>
+        <div class="float-right">
+          <button class="btn btn-success" @click="buyStock">Buy</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      quantity: 0
+    }
+  },
+  props: ['stock'],
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      }
+      this.quantity = 0      
+    }
+  }
+}
+</script>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>The Stocks components</h1>
+  <div class="row">
+    <app-stock v-for="(stock, idx) in stocks" :key="idx" :stock='stock'></app-stock>
+  </div>
 </template>
+<script>
+import Stock from './Stock'
+export default {
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: 'HMSP', price: 3050 },
+        { id: 2, name: 'ICBP', price: 650 },
+        { id: 3, name: 'ASII', price: 110 },
+        { id: 4, name: 'TLKM', price: 2350 },
+      ]
+    }
+  },
+  components: {
+    appStock: Stock
+  }
+}
+</script>
