e767ff00deeda3e5481f3bc2dc850c0e2b460041
ruslansleekr <ruslan@sleekr.co>
2019-07-22 21:52:38 +0700

    setup portfolio & button sell

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,3 +1,50 @@
 <template>
-  <h1>The Portfolio - stock components</h1>
+  <div class="col-sm-6 col-md-4 mb-3">
+    <div class="card">
+      <div class="card-header text-left bg-success text-white">
+        <p class="mb-0">
+          {{ stock.name }}
+          <span>(Price: {{ stock.price }} | Quantity: {{ stock.quantity }})</span>
+        </p>
+      </div>
+      <div class="card-body">
+        <div class="float-left">
+          <input type="number"
+                  class="form-control"
+                  placeholder="Quantity"
+                  v-model="quantity">
+        </div>
+        <div class="float-right">
+          <button class="btn btn-success"
+                  @click="sellStock"
+                  :disabled="quantity <= 0">Sell</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<script>
+import { mapActions } from 'vuex'
+export default {
+  props: ['stock'],
+  data() {
+    return {
+      quantity: 0
+    }
+  },
+  methods: {
+    ...mapActions({
+      placeSellOrder: 'sellStock'
+    }),
+    sellStock () {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity,
+      }
+      this.placeSellOrder(order)
+      this.quantity = 0
+    }
+  }
+}
+</script>
--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -18,6 +18,7 @@ const mutations = {
   },
   'SELL_STOCK'(state, {stockId, quantity, stockPrice}) {
     const record = state.stocks.find(element => element.id == stockId)
+    debugger
     if (record.quantity > quantity) {
       record.quantity -= quantity
     } else {
