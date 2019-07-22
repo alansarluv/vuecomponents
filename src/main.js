c77bffc60e170b54f4cc7dfdfa77d0a3e94cb37c
ruslansleekr <ruslan@sleekr.co>
2019-07-23 00:25:10 +0700

    setup firebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import { routes } from './routes'
 
+import VueResource from 'vue-resource'
+
 import store from './store/store'
 
 Vue.use(VueRouter)
@@ -11,6 +13,9 @@ const router = new VueRouter({
   mode: 'history',
   routes
 })
+
+Vue.use(VueResource)
+Vue.http.options.root = 'https://vuejs-stock-trader-669f1.firebaseio.com/'
 
 Vue.filter ('currency', (value) => {
   return 'Rp. ' + value.toLocaleString()
